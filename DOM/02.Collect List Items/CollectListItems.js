0da385a6171966950bbabe0a9b2ed1d2cb29eab1
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-01-26 02:00:49 +0200

    Add files via upload

---
+++ DOM/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,6 @@
+function extractText() {
+    let items = Array.from(document.querySelectorAll('li'));
+    let result = items.map(e => e.textContent).join('\n');
+
+    document.getElementById('result').value = result;
+}
