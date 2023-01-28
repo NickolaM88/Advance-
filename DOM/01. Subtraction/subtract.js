259e65c2bd5db0a86b78666bef3ac578b2404949
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-01-28 20:43:53 +0200

    Add files via upload

---
+++ DOM/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let first = document.getElementById('firstNumber').value;
+    let second = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+
+    result.textContent = +first - +second;
+}
