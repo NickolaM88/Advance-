322a9bdfbda3de171feafe717916de47292ff284
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-02-19 03:37:48 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements/08. Days in a month.js
@@ -0,0 +1,8 @@
+function getDaysInMonth(month,year) {
+    // Here January is 1 based
+    //Day 0 is the last day in the previous month
+   return new Date(year, month, 0).getDate();
+  // Here January is 0 based
+  // return new Date(year, month+1, 0).getDate();
+  };
+  console.log(getDaysInMonth(2, 2021));
