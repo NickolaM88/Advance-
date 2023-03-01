c16d430c44273d6f7f9ee7d82acf1cfead4c860f
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-03-02 00:02:50 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements/05. Time to Walk.js
@@ -0,0 +1,22 @@
+function solve(steps, footprint, speed) {
+  let distanceMeters = steps * footprint;
+  let speedMetersSec = speed / 3.6;
+  let time = distanceMeters / speedMetersSec;
+  let rest = Math.floor(distanceMeters / 500);
+
+  let timeMin = Math.floor(time / 60);
+  let timeSec = Math.round(time - timeMin * 60);
+  let timeHour = Math.floor(time / 3600);
+
+  console.log(
+    (timeHour < 10 ? "0" : "") +
+      timeHour +
+      ":" +
+      (timeMin + rest < 10 ? "0" : "") +
+      (timeMin + rest) +
+      ":" +
+      (timeSec + rest < 10 ? "0" : "") +
+      timeSec
+  );
+}
+solve(4000, 0.6, 5);
