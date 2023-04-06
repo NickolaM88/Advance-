21e27225f4d5e837f5b354d0b6da58df33ff03da
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-04-07 01:48:59 +0300

    Add files via upload

---
+++ Prototypes and Inheritance/04. Extend Prototype.js
@@ -0,0 +1,6 @@
+function extendedPrototype(classDef) {
+    classDef.prototype.species = 'Human';
+    classDef.prototype.toSpeciesString = function() {
+        return `I am a ${this.species}. ${this.toString()}`;
+    }
+}
